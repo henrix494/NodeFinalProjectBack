a8d78b42c32380942982541f1bee0a3cff104b0b
henrix494 <henrix494@gmail.com>
2024-06-15 15:21:27 +0300

    added db connect

--- dal/configProd.js
+++ dal/configProd.js
@@ -4,7 +4,7 @@ export const ProdConfig = {
   host: process.env.DB_HOST,
   dialect: "mssql",
 
-  username: process.env.DB_USERNAME,
+  username: process.env.POSTGRES_USER,
   password: process.env.DB_PASSWORD,
 
   database: process.env.DB_DATABASE,
