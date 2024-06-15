fdc5af9abda754d62f3636529c235b1cede397ed
henrix494 <henrix494@gmail.com>
2024-06-15 14:58:50 +0300

    first commit

--- dal/db.js
+++ dal/db.js
@@ -1,7 +1,9 @@
 import { Sequelize, DataTypes } from "sequelize";
+import mysql2 from "mysql2"; // Needed to fix sequelize issues with WebPack
 const sequelize = new Sequelize("restauranti", "root", "root", {
   host: "localhost",
   dialect: "mysql",
+  dialectModule: mysql2,
   logging: false,
 });
 
