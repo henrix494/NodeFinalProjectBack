409a9e446163f9b929ab21e228883e60fff2b82d
henrix494 <henrix494@gmail.com>
2024-06-15 15:15:20 +0300

    added db connect

--- dal/configProd.js
+++ dal/configProd.js
@@ -2,7 +2,7 @@ import * as tedious from "tedious";
 
 export const ProdConfig = {
   host: process.env.DB_HOST,
-  dialect: "mysql",
+  dialect: "mssql",
 
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
